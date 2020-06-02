8c3a6849aba22f6bd51ef555c25a3e4df35e7e5b
Greg Fenton <greg.fenton@gmail.com>
2020-06-02 12:52:16 -0400

    minor formatting cleanup

--- example/src/App.js
+++ example/src/App.js
@@ -6,9 +6,6 @@ import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import ReactFirestoreQueryLab from "react-firestore-query-lab";
 
 import { configureStore } from "./firestoreConfig";
-
-// Setup react-redux so that connect HOC can be used
-
 const [store, rrfProps] = configureStore();
 
 function App() {
